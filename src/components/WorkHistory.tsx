5c5bee9396dcf7c2acfedd1ec0b4b876526c562b
Mark Pavel <markpavel02@gmail.com>
2023-02-18 16:28:43 -0500

    Fixed Work History: List Disc, Layout, Border

--- src/components/WorkHistory.tsx
+++ src/components/WorkHistory.tsx
@@ -8,146 +8,86 @@ export default function WorkHistory(workData: WorkData) {
         // Current box being added
         let currentBox: Array<ReactNode> = [];
 
+        const getHistory = (entry: WorkHistoryData, index: number) => {
+            return <li key={index} className="list-disc list-inside p-4 w-1/2 h-1/2 border-cyan-900 first:border-y-2 last:border-x-2 odd:border-r-2 even:border-y-2">
+                <article className="flex flex-col items-stretch w-full h-full">
+                    <aside className="flex m-3 h-1/2">
+                        <div className="flex flex-col m-1 items-start justify-center w-1/2">
+                            <h2 className="text-3xl">{entry.employer}</h2>
+
+                            <h3 className="text-2xl">{entry.title}</h3>
+
+                            <h4 className="text-xl">{entry.time.start} - {entry.current ? "Current" : entry.time.end}</h4>
+
+                            <h4 className="text-lg">{entry.local}</h4>
+                        </div>
+
+                        <div className="flex flex-col m-3 items-stretch justify-evenly w-1/2">
+                            <h4 className="text-3xl">Tasks</h4>
+
+                            <ul>
+                                {entry.tasks.map((task, index) => {
+                                    return <li key={index}
+                                        className="m-1"
+                                    >
+                                        <p className="text-xl">
+                                            {task}
+                                        </p>
+                                    </li>
+                                })}
+                            </ul>
+                        </div>
+                    </aside>
+
+                    <aside className="flex m-3 h-1/2">
+                        <div className="flex flex-col m-3 items-stretch w-1/2">
+                            <h4 className="text-3xl">Day To Day</h4>
+
+                            <ul>
+                                {entry.dayToDay.map((day, index) => {
+                                    return <li key={index}
+                                        className="m-1"
+                                    >
+                                        <p className="text-xl">
+                                            {day}
+                                        </p>
+                                    </li>
+                                })}
+                            </ul>
+                        </div>
+
+                        <div className="flex flex-col m-3 items-center w-1/2">
+                            <h4 className="text-3xl m-3">Key Points</h4>
+
+                            <ul>
+                                {entry.keyPoints.map((point, index) => {
+                                    return <li key={index}
+                                        className="m-1"
+                                    >
+                                        <p className="text-xl">
+                                            {point}
+                                        </p>
+                                    </li>
+                                })}
+                            </ul>
+                        </div>
+                    </aside>
+                </article>
+            </li>
+        }
+
         // Loops over the tools
         entries.forEach((entry, index) => {
             // Checks to see if the current box is filled
             if (currentBox.length < total) {
                 // If not then it pushes the li to the current box
-                currentBox.push(<li key={index} className="list-disc border-cyan-900 odd:border-r-2 even:border-y-2 first:border-y-2 last:border-l-2 w-1/2">
-                    <article className="flex flex-col items-center">
-                        <aside className="flex m-3">
-                            <div className="flex flex-col m-1 items-start justify-center w-1/2">
-                                <h2 className="text-3xl">{entry.employer}</h2>
-
-                                <h3 className="text-2xl">{entry.title}</h3>
-
-                                <h4 className="text-xl">{entry.time.start} - {entry.current ? "Current" : entry.time.end}</h4>
-
-                                <h4 className="text-lg">{entry.local}</h4>
-                            </div>
-
-                            <div className="flex flex-col m-2 items-center justify-evenly w-1/2">
-                                <h4 className="text-3xl">Tasks</h4>
-
-                                <ul>
-                                    {entry.tasks.map((task, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {task}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-                        </aside>
-
-                        <aside className="flex m-3">
-                            <div className="flex flex-col m-2 items-center w-1/2">
-                                <h4 className="text-3xl">Day To Day</h4>
-
-                                <ul>
-                                    {entry.dayToDay.map((day, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {day}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-
-                            <div className="flex flex-col m-2 items-center w-1/2">
-                                <h4 className="text-3xl">Key Points</h4>
-
-                                <ul>
-                                    {entry.keyPoints.map((point, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {point}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-                        </aside>
-                    </article>
-                </li>);
+                currentBox.push(getHistory(entry, index));
                 // If it is, then it adds it to the boxes and clears the current box 
             } else {
                 boxes.push(currentBox)
                 currentBox = [];
 
-                currentBox.push(<li key={index} className="list-disc border-cyan-900 odd:border-r-2 even:border-y-2 first:border-y-2 last:border-l-2 w-1/2">
-                    <article className="flex flex-col items-center w-1/2">
-                        <aside className="flex m-3">
-                            <div className="flex flex-col m-1 items-start justify-center w-1/2">
-                                <h2 className="text-3xl">{entry.employer}</h2>
-
-                                <h3 className="text-2xl">{entry.title}</h3>
-
-                                <h4 className="text-xl">{entry.time.start} - {entry.current ? "Current" : entry.time.end}</h4>
-
-                                <h4 className="text-lg">{entry.local}</h4>
-                            </div>
-
-                            <div className="flex flex-col m-2 items-center justify-evenly w-1/2">
-                                <h4 className="text-3xl">Tasks</h4>
-
-                                <ul>
-                                    {entry.tasks.map((task, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {task}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-                        </aside>
-
-                        <aside className="flex m-3">
-                            <div className="flex flex-col m-2 items-center w-1/2">
-                                <h4 className="text-3xl">Day To Day</h4>
-
-                                <ul>
-                                    {entry.dayToDay.map((day, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {day}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-
-                            <div className="flex flex-col m-2 items-center w-1/2">
-                                <h4 className="text-3xl">Key Points</h4>
-
-                                <ul>
-                                    {entry.keyPoints.map((point, index) => {
-                                        return <li key={index}
-                                            className="m-1"
-                                        >
-                                            <p className="text-xl">
-                                                {point}
-                                            </p>
-                                        </li>
-                                    })}
-                                </ul>
-                            </div>
-                        </aside>
-                    </article>
-                </li>);
+                currentBox.push(getHistory(entry, index));
             }
 
             if (currentBox.length === total) {
