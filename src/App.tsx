60e93f60d2235d551f4a73f5b631e5527f339153
Mark Pavel <markpavel02@gmail.com>
2023-02-21 16:17:22 -0500

    Added Page Background

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,18 @@ import { data } from "./data";
 
 function App() {
   return (
-    <main className="flex flex-col items-center m-4 overflow-hidden">
-      <div className="flex w-screen relative layered-peaks before:absolute before:-z-10 before:bg-center before:bg-cover before:bg-no-repeat before:opacity-60 before:content-[''] before:w-full before:h-full border-cyan-900 shadow-md shadow-cyan-900">
-        <Header {...data.header} />
-        <Details {...data.personalDetails} />
-      </div>
-      <Skills {...data.skills} />
-      <WorkHistory {...data.workHistory} />
-      <Education {...data.education} />
-      <Projects {...data.projects} />
-    </main>
+    <div className="background bg-center bg-cover bg-no-repeat p-2">
+      <main className="flex flex-col items-center m-4 overflow-hidden">
+        <div className="flex w-screen relative layered-peaks before:absolute before:-z-10 before:bg-center before:bg-cover before:bg-no-repeat before:opacity-60 before:content-[''] before:w-full before:h-full border-cyan-900 shadow-md shadow-cyan-900">
+          <Header {...data.header} />
+          <Details {...data.personalDetails} />
+        </div>
+        <Skills {...data.skills} />
+        <WorkHistory {...data.workHistory} />
+        <Education {...data.education} />
+        <Projects {...data.projects} />
+      </main>
+    </div>
   )
 }
 
