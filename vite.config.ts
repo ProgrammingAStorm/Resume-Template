d1378a234aa5c0ccad18fbbbdce1ff3ae5ecffa3
Mark Pavel <markpavel02@gmail.com>
2023-02-21 13:19:05 -0500

    Removed SVG Loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import svgLoader from 'vite-svg-loader'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader()],
+  plugins: [react()],
 })
